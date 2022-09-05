3c51a07d8b0430c59d5a8ffe47a5a8c485d8b202
Журавлёв Иван <bosk@yandex.ru>
2022-09-05 21:33:43 +0300

    realized task 0

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// Напишите программу, которая на вход принимает число и выдает его квадрат.
+
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
